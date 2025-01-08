7d56a727cfd11c603b2bf2d80d94e667e5f142b1
Mike Carew <mike@carew.uk>
2025-01-08 21:26:13 +0000

    index.js

--- setup-ts-project.sh
+++ setup-ts-project.sh
@@ -19,13 +19,11 @@ cat > tsconfig.json <<EOF
     "target": "es6",
     "module": "amd",
     "strict": true,
-    "outFile": "./dev/main.js",
     "outDir": "./dev",
     "rootDir": "./src",
     "sourceMap": true,
     "esModuleInterop": true,
-    "forceConsistentCasingInFileNames": true,
-    "sourceRoot": "/src"
+    "forceConsistentCasingInFileNames": true
   },
   "include": ["src/**/*"],
   "exclude": ["node_modules", "dist"]
@@ -46,7 +44,7 @@ cat > index.html <<EOF
     <title>TypeScript Project</title>
 </head>
 <body>
-    <script src="dev/main.js"></script>
+    <script src="dev/index.js"></script>
 </body>
 </html>
 EOF
